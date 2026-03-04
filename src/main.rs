cd0d3589cb1bd001075ac0e4394ad28ee810b622
CorneliusCornbread <jrosenbeckerdev@gmail.com>
2026-03-04 16:24:36 -0600

    Basic listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello World! " }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80")
+        .await
+        .expect("Unable to bind to port 80");
+    axum::serve(listener, app).await.unwrap();
 }
