4e9a81c64f2d19e63349c26276ebb56d42ee3c99
CorneliusCornbread <jrosenbeckerdev@gmail.com>
2026-03-25 16:33:43 -0500

    Basic hello world with logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use axum::{Router, routing::get};
+use axum::Router;
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
+
     let app = Router::new().fallback_service(ServeDir::new("static"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:800")
         .await
         .expect("Unable to bind to port 800");
+
+    tracing::info!("Serving on http://{}", listener.local_addr().unwrap());
+
     axum::serve(listener, app).await.unwrap();
 }
