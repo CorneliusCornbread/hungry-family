de561bb13aab455b9a12fedbab4826a603a3b1da
CorneliusCornbread <jrosenbeckerdev@gmail.com>
2026-03-25 16:21:20 -0500

    Basic hello world!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use axum::{Router, routing::get};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello World! " }));
+    let app = Router::new().fallback_service(ServeDir::new("static"));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:80")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:800")
         .await
-        .expect("Unable to bind to port 80");
+        .expect("Unable to bind to port 800");
     axum::serve(listener, app).await.unwrap();
 }
